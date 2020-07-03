86656e64802210831d82ded78c04764999eae80b
kamleshsilag <kamleshsilag42@gmail.com>
2020-07-03 21:09:11 +0530

    Calling External API

--- movie-catalog-service/src/main/java/io/kamlesh/moviecatalogservice/resource/MovieCatalogResource.java
+++ movie-catalog-service/src/main/java/io/kamlesh/moviecatalogservice/resource/MovieCatalogResource.java
@@ -32,7 +32,7 @@ public class MovieCatalogResource {
         //Movie info API
         return ratings.getUserRating().stream().map(rating -> {
             Movie movie = restTemplate.getForObject("http://movie-info-service/movies/" + rating.getMovieId(), Movie.class);
-            return new CatalogItem(movie.getName(),"Description",rating.getRating());
+            return new CatalogItem(movie.getName(),movie.getDescription(),rating.getRating());
         }).collect(Collectors.toList());
     }
 }
